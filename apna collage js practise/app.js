da35b667f43359a827cdcce6e155bab9af2c0b75
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-03-07 09:44:41 +0500

    practise

---
+++ apna collage js practise/app.js
@@ -0,0 +1,54 @@
+//artimetic operators
+// let a = 5;
+// let b = 2;
+// console.log("a = ", a, " & b = ", b  );
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a / b =", a / b);
+// console.log("a * b =", a * b);
+
+// modulus 
+// console.log("a % b =", a % b);
+
+
+//exponentiation
+// console.log("a ** b =", a ** b);// means 5 ka power 2 
+
+//Unary operators
+// let a = 5;
+// let b = 2;
+// increment op
+// 2 tariqe plus aur minus k lye 
+// console.log("a = ", a, " & b = ", b  );
+//  a = a + 1;         // a = 6 
+// console.log("a =" , a);
+
+//plus ka shrtcut triqa
+//a = 7
+// a++;
+// console.log("a =" , a);  
+//decrement op
+
+// a--: 1 minus krega nmbr se
+
+// Assignmnt opreators
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 4     a braber hae a = 4 k
+// console.log("a = ", a); // 9  same m- d/ m*  %modulus  **exponentiation krsktehe 
+
+
+//Camparison operators
+let a = 5;
+let b = 2;
+
+// console.log("5 == 2 ", a==b);// false aega aur true agr equal uve
+// console.log("5 != 2 ", a!=b);  // != is ka matlab 5 & 2 equal nhehe agr equal hote to false ata 
+// console.log("5 === 2 ", a === b);
+// console.log("5 !== 2 ", a!==b);
+ // greater and less
+ console.log("5 > 2 ", a > b); //true
+ console.log("5 < 2 ", a < b); //false
+ 
